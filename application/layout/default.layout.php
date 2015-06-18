9b8e2762b516619d60ffaf262a6c58795c956afb
Esysteme <aurelien.lequoy@esysteme.com>
2015-06-19 01:00:47 +0200

    add graphiz lib patched

--- application/layout/default.layout.php
+++ application/layout/default.layout.php
@@ -3,22 +3,24 @@
 use \Glial\Synapse\FactoryController;
 use \Glial\I18n\I18n;
 
-FactoryController::addNode("Layout", "header",$GLIALE_TITLE);
+FactoryController::addNode("Layout", "headerPma",$GLIALE_TITLE);
+
 
 
 echo '<div id="page">';
 
-echo "<div id=\"title\">";
-echo "<h2>".$GLIALE_TITLE."<br />";
-echo "<span><a href=\"".WWW_ROOT."\">".__("Home")."</a> ".$GLIALE_ARIANE."</span>";
-echo "</h2>";
+echo "<div id=\"glial-title\">";
+echo "<h2>".$GLIALE_TITLE."</h2>";
+echo "<span class=\"ariane\"><a href=\"".WWW_ROOT."\">".__("Home")."</a> ".$GLIALE_ARIANE."</span>";
 echo "</div>";
+
+
+echo "<div style=\"padding:0 10px 10px 10px\">";
 
 get_flash();
 echo $GLIALE_CONTENT;
-
-
+echo "</div>";
 echo '</div>';
 
 
-FactoryController::addNode("Layout", "footer");
+FactoryController::addNode("Layout", "footerPma");
