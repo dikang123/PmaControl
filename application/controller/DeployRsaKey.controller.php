57e1546cdab78d3bb7b10617572f9dfd1528d001
Esysteme <timansl@hotmail.com>
2016-09-21 19:31:49 +0200

    version of deploy key RSA

--- application/controller/DeployRsaKey.controller.php
+++ application/controller/DeployRsaKey.controller.php
@@ -12,10 +12,109 @@ class DeployRsaKey extends Controller
 
     public function index()
     {
-
-
         $this->title  = '<i style="font-size: 32px" class="fa fa-key" aria-hidden="true"></i> '."Deploy key RSA";
         $this->ariane = '> <i style="font-size: 16px" class="fa fa-puzzle-piece"></i> Plugins > '
             .'<i style="font-size: 16px" class="fa fa-key" aria-hidden="true"></i> '."Deploy key RSA";
+
+        $db = $this->di['db']->sql(DB_DEFAULT);
+
+        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+            if (!empty($_POST['settings'])) {
+
+                debug($_POST);
+
+                $list_id = [];
+                foreach ($_POST['id'] as $key => $value) {
+
+                    if (!empty($_POST['mysql_server'][$key]["is_monitored"]) && $_POST['mysql_server'][$key]["is_monitored"] === "on")
+                    {
+                        $list_id[] = $value;
+                    }
+                }
+
+
+                $ids = implode(",",$list_id);
+
+                $sql = "SELECT * FROM mysql_server WHERE id IN (".$ids.")";
+                $res = $db->sql_query($sql);
+
+
+                shell_exec("mkdir ".DATA."keys/");
+
+                while ($ob = $db->sql_fetch_object($res))
+                {
+                    $cmd ='ssh-keygen -t rsa -N "" -b 4096 -C "PmaControl@esysteme.com" -f '.DATA."/keys/".$ob->ip.'.key';
+                    shell_exec($cmd);
+                    
+                }
+
+                debug($list_id);
+
+                //header("location: ".LINK."DeployRsaKey/index/");
+                //exit;
+            }
+        }
+
+
+
+        $this->title  = '<i class="fa fa-server"></i> '.__("Servers");
+        $this->ariane = ' > <a href⁼"">'.'<i class="fa fa-cog" style="font-size:14px"></i> '
+            .__("Settings").'</a> > <i class="fa fa-server"  style="font-size:14px"></i> '.__("Servers");
+
+
+
+        $sql             = "SELECT * FROM mysql_server a WHERE 1=1 ".$this->getFilter()." ORDER by name";
+        $data['servers'] = $db->sql_fetch_yield($sql);
+
+
+
+
+
+        $this->set('data', $data);
+    }
+
+    //to mutualize
+    private function getFilter()
+    {
+
+        $where = "";
+
+
+        if (!empty($_GET['environment']['libelle'])) {
+            $environment = $_GET['environment']['libelle'];
+        }
+        if (!empty($_SESSION['environment']['libelle']) && empty($_GET['environment']['libelle'])) {
+            $environment                    = $_SESSION['environment']['libelle'];
+            $_GET['environment']['libelle'] = $environment;
+        }
+
+        if (!empty($_SESSION['client']['libelle'])) {
+            $client = $_SESSION['client']['libelle'];
+        }
+        if (!empty($_GET['client']['libelle']) && empty($_GET['client']['libelle'])) {
+            $client                    = $_GET['client']['libelle'];
+            $_GET['client']['libelle'] = $client;
+        }
+
+
+        if (!empty($environment)) {
+            $where .= " AND a.id_environment IN (".implode(',', json_decode($environment, true)).")";
+        }
+
+        if (!empty($client)) {
+            $where .= " AND a.id_client IN (".implode(',', json_decode($client, true)).")";
+        }
+
+
+        return $where;
+    }
+
+
+    private function deploy($ip, $login, $password, $path_public_key)
+    {
+
+
+        
     }
 }
--- application/view/DeployRsaKey/index.view.php
+++ application/view/DeployRsaKey/index.view.php
@@ -1,5 +1,8 @@
 <?php
-/* 
+
+use Glial\Html\Form\Form;
+
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -21,39 +24,42 @@ echo '<div class="well">';
 \Glial\Synapse\FactoryController::addNode("Common", "displayClientEnvironment", array());
 echo '</div>';
 
-
-
-
-
-echo '<form action="" method="POST">';
+echo '<form action="" method="post" class="form-inline">';
 echo '<div class="well">';
-\Glial\Synapse\FactoryController::addNode("Common", "displayClientEnvironment", array());
-echo '<br />';
-echo ' <a href="'.LINK.'Mysql/add/" class="btn btn-primary" style="font-size:12px"><span class="glyphicon glyphicon-plus" style="font-size:12px"></span> Add a MySQL server</a> ';
-echo '</div>';
-echo '</form>';
 
-echo '<form action="" method="POST">';
+
+echo '<div class="form-group">';
+echo "Login SSH : ";
+echo Form::input("mysql_server", "login_ssh", array("class"=>"form-control", "style"=>"width:200px"));
+
+echo " - Password SSH : ";
+echo Form::input("mysql_server", "password_ssh", array("class"=>"form-control", "style"=>"width:200px"));
+
+
+echo ' <button type="submit" class="btn btn-primary">Deploy</button>';
+echo '</div>';
+
+echo '</div>';
+
+
 echo '<table class="table table-bordered table-striped" id="table">';
 echo '<tr>';
+echo '<th><input id="checkAll" type="checkbox" onClick="toggle(this)" /> '.__("Deploy").'</th>';
 echo '<th>'.__('Top').'</th>';
 echo '<th>'.__('ID').'</th>';
 echo '<th>'.__('MySQL').'</th>';
 echo '<th>'.__('SSH').'</th>';
-echo '<th><input id="checkAll" type="checkbox" onClick="toggle(this)" /> '.__("Monitored").'</th>';
-
-//echo '<th>'.__('Monitored').'</th>';
 echo '<th>'.__('Client').'</th>';
 echo '<th>'.__('Environment').'</th>';
+
 echo '<th>'.__('Tags').'</th>';
 echo '<th>'.__('Name').'</th>';
-echo '<th>'.__('Display name').'</th>';
 echo '<th>'.__('IP').'</th>';
 echo '<th>'.__('Port').'</th>';
 
 echo '</tr>';
 
-$i = 0;
+$i     = 0;
 $style = '';
 
 Form::setIndice(true);
@@ -62,10 +68,16 @@ foreach ($data['servers'] as $server) {
 
     $i++;
     echo '<tr>';
+
+    $checked = $server['is_monitored'] == 1 ? 'checked="checked"' : '';
+
+    echo '<td style="'.$style.'">'
+    .'<input type="checkbox" name="mysql_server['.($i - 1).'][is_monitored]" '.$checked.' />'.'</td>';
+
     echo '<td>'.$i.'</td>';
     echo '<td>'.$server['id'];
     //print_r($server);
-    echo '<input type="hidden" name="id['.($i-1).']" value="'.$server['id'].'" />';
+    echo '<input type="hidden" name="id['.($i - 1).']" value="'.$server['id'].'" />';
 
     echo '</td>';
 
@@ -78,32 +90,14 @@ foreach ($data['servers'] as $server) {
     echo '</td>';
 
 
-    $checked = $server['is_monitored'] == 1 ? 'checked="checked"':   '';
-
-    echo '<td style="'.$style.'">'
-
-        .'<input type="checkbox" name="mysql_server['.($i-1).'][is_monitored]" '.$checked.' />'.'</td>';
-
-    echo '<td>';
-    echo Form::select("mysql_server", "id_client", $data['clients'], $server['id_client'], array());
-    echo '</td>';
+    echo '<td>'.$server['name'].'</td>';
+    echo '<td>'.$server['name'].'</td>';
 
 
-    echo '<td>';
-    echo Form::select("mysql_server", "id_environment", $data['environments'], $server['id_environment'], array());
-    echo '</td>';
     echo '<td>'.__('Tags').'</td>';
     echo '<td>'.$server['name'].'</td>';
 
 
-    if (empty($server['display_name']))
-    {
-        $server['display_name'] = $server['name'];
-    }
-
-
-    $_GET["mysql_server"][($i-1)]["display_name"] = $server['display_name'];
-    echo '<td>'.Form::input("mysql_server", "display_name").'</td>';
     echo '<td>'.$server['ip'].'</td>';
     echo '<td>'.$server['port'].'</td>';
     echo '</tr>'."\n";
@@ -114,5 +108,5 @@ Form::setIndice(false);
 echo '</table>';
 
 echo '<input type="hidden" name="settings" value="1" />';
-echo '<button type="submit" class="btn btn-primary">'.__("Update").'</button>';
+echo '<button type="submit" class="btn btn-primary">'.__("Deploy").'</button>';
 echo '</form>';
